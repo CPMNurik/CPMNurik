66791abca6039e505271b0d283b916560b072027
CPMNurik <erbolatsultan777@gmail.com>
2025-01-19 06:59:16 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from cpmnurik import CPMNurik
+from NurikCPM.py import CPMNurik.py
 
 __CHANNEL_USERNAME__ = "CPMNurikchannel"
 __GROUP_USERNAME__   = "CPMNurikchat"
